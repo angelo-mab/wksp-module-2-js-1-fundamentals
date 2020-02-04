62615199cb7f0cd4c626d450ea7e14ecea3e3540
angelo-mab <60443712+angelo-mab@users.noreply.github.com>
2020-02-04 10:02:51 -0500

    Update 5-exercise-9.js

--- workshop/5-control-flow/5-exercise-9.js
+++ workshop/5-control-flow/5-exercise-9.js
@@ -18,7 +18,29 @@
 // #_#_#_#_
 // _#_#_#_#
 
+// for (let i = 1; i <= 8; i++) {
+//   if (i % 2 == 1) console.log("#_#_#_#_");
+//   else console.log("_#_#_#_#");
+// }
+
 for (let i = 1; i <= 8; i++) {
-  if (i % 2 == 1) console.log("#_#_#_#_");
-  else console.log("_#_#_#_#");
+  let line = "";
+  if (i % 2 === 0) {
+    for (let k = 1; k <= 8; k++) {
+      if (k % 2 === 0) {
+        line += "_";
+      } else {
+        line += "#";
+      }
+    }
+  } else {
+    for (j = 1; j <= 8; j++) {
+      if (j % 2 === 0) {
+        line += "#";
+      } else {
+        line += "_";
+      }
+    }
+  }
+  console.log(line);
 }
